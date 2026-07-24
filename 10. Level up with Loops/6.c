9dfb385b3373fdd760cd2c1eb5831fa50281b5c1
Aman Shah <10xdevaman@gmail.com>
2026-07-24 20:51:36 +0530

    Program 6 : To print all prime numbers under 100.

---
+++ 10. Level up with Loops/6.c
@@ -0,0 +1,24 @@
+// Write a program to print all Prime number under 100.
+
+#include <stdio.h>
+
+int main() {
+    int i, j, isPrime;
+
+    printf("Prime numbers under 100 are: ");
+
+    for(i = 2; i < 100; i++) {
+        isPrime = 1;
+        for(j = 2; j < i; j++) {
+            if(i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if(isPrime == 1) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
