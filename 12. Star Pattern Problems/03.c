23c7dfd087162eea902c07d2dc6cc48af4d13d24
Aman Shah <10xdevaman@gmail.com>
2026-07-26 19:53:03 +0530

    Program 03. To draw pattern 13.

---
+++ 12. Star Pattern Problems/03.c
@@ -0,0 +1,25 @@
+// Write a program to draw below pattern.
+/*
+   *
+  * *
+ * * *
+* * * *
+*/
+
+
+#include <stdio.h>
+int main() {
+    int i, j, k;
+    int n = 4;
+
+    for (i = 1; i <= n; i++) {
+        for (j = n; j > i; j--) {
+            printf(" ");
+        }
+        for (k = 1; k <= i; k++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
