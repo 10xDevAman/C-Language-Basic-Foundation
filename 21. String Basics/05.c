e70ee0796b30bbb3b3d7dce495c13398825926e1
Aman Shah <10xdevaman@gmail.com>
2026-08-04 19:34:41 +0530

    Program 5. To convert a given string into uppercase.

---
+++ 21. String Basics/05.c
@@ -0,0 +1,26 @@
+// Write a program to convert a given string into uppercase.
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while(str[i] != '\0')
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+
+        i++;
+    }
+
+    printf("\nUppercase String: %s", str);
+
+    return 0;
+}
