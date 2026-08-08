568406439cda1c2d4143a87a349d92ac649b74d3
Aman Shah <10xdevaman@gmail.com>
2026-08-08 09:36:07 +0530

    Program 1 : Function to swap values of two int variables.

---
+++ 25. Pointers/01.c
@@ -0,0 +1,31 @@
+// Write a function to swap values of two int variables. (TSRN)
+
+
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main()
+{
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+
+    swap(&x, &y);
+
+    printf("After swapping:  x = %d, y = %d\n", x, y);
+
+    return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
