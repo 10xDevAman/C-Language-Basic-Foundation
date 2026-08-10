d06058943e3d270602b4a669eeb7a07947a7dbde
Aman Shah <10xdevaman@gmail.com>
2026-08-10 16:45:24 +0530

    Program 1 : Define a structure Employee with member variable id, name, salary.

---
+++ 26. Structure/01.c
@@ -0,0 +1,17 @@
+// Define a structure Employee with member variables id, name, salary.
+
+#include <stdio.h>
+
+struct Employee
+{
+    int id;
+    char name[50];
+    float salary;
+};
+
+int main()
+{
+    struct Employee emp;
+
+    return 0;
+}
